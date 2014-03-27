381b92462dee38da89b539e4a770bc1ef0c0b75b
alessandro40 <alessandro.40@alice.it>
2014-03-27 16:56:22 +0100

    Now libneubotjava.so can be built succesfully with ndk-build.
    In the root folder of the project there must be the file project.properties,
    where the Android API version target is declared. Otherwise, ndk-build does not
    include a folder containing some header files necessary to the use of libevent.

---
+++ jni/Android.mk
@@ -0,0 +1,17 @@
+TOP_LOCAL_PATH:=$(call my-dir)
+
+include $(call all-subdir-makefiles)
+
+
+LOCAL_PATH:=$(TOP_LOCAL_PATH)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := neubot_wrap.cxx
+		
+LOCAL_MODULE := neubotjava
+
+LOCAL_STATIC_LIBRARIES := neubot
+
+include $(BUILD_SHARED_LIBRARY)
+
+
